bfa373f9341c237bc2a97d137ceda66cd85ca04d
yarah-sf <yarahs669@gmail.com>
2025-04-24 10:48:17 -0400

    corrigindo exercicios

--- exercicios/aula3/aula3-exercicio1.js
+++ exercicios/aula3/aula3-exercicio1.js
@@ -2,7 +2,17 @@
 
 let num = 10;
 let num2 = 2;
-console.log(num + num2); 
-console.log(num - num2);
+let soma = num + num2
+let subtracao = num - num2
+let multiplic = num * num2
+let divisao = num / num2
+console.log(`
+    ${num} + ${num2} = ${soma};
+    ${num} - ${num2} = ${subtracao};
+    ${num} * ${num2} = ${multiplic};
+    ${num} / ${num2} = ${divisao}.
+    `); 
+
+`console.log(num - num2);
 console.log(num * num2);
-console.log(num / num2);
+console.log(num / num2);`
